cc22018d72d6796bbbf4855adc0a22f17ab1b92c
iloveket <davidrb2017@outlook.com>
2020-03-23 12:07:38 +0000

    .env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: 'api.openweathermap.org/data/2.5/'
+};
+
 function App() {
   return <div className="app"></div>;
 }
+
+console.log(api.key);
 
 export default App;
